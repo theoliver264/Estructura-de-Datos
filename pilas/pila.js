c17195d079abb8551d0e50748c8a6d7cd296e7d5
20140986 RIOS FRIAS OLIVER MAXIMILIANO <20140986@TELEMATICA.UCOL.MX>
2018-09-26 11:37:34 -0500

    Push and pop

---
+++ pilas/pila.js
@@ -0,0 +1,35 @@
+class Pila {
+    constructor(size){
+        this.maxSize = size
+        this.tope = 0;
+        this.pila = new Array(size)
+    }
+
+    push(x){
+        if(this.tope < this.maxSize){
+            this.tope++
+            this.pila[this.tope-1] = x
+        } else throw "Stack Overflow"
+    }
+
+    pop(){
+        let dato
+        if(this.tope > 0){
+            dato = this.pila[this.tope-1]
+            this.pila[this.tope-1] = null
+            this.tope--
+            this.print()
+            return dato
+        } else throw "Stack Underflow"
+    }
+
+    print(){
+        for(let i = this.maxSize - 1; i >= 0; i--){
+            console.log("[ "+ this.pila[i] +" ]")
+        }
+    }
+}
+
+let stack = new Pila(10)
+for(let i = 1; i<=10;i++) stack.push(i)
+stack.print()
